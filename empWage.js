dc7e255b1653daca8546a8ddf694ce68a7d9a97f
Tejas <tejasdev95@gmail.com>
2021-04-11 15:35:56 +0530

    Stored dailyWage in array

--- empWage.js
+++ empWage.js
@@ -6,6 +6,12 @@ const WAGE_PER_HR = 20;
 const NO_OF_WORKING_DAYS = 20;
 const MAX_WORKING_HRS = 160;
 
+let empWage;
+let TotalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+let empHrs = 0;
+
 function getEmpHrs(empCheck){
     switch(empCheck) {
     case IS_FULLTIME:
@@ -19,17 +25,20 @@ function getEmpHrs(empCheck){
     }
 }
 
-
-let empWage;
-let TotalEmpHrs = 0;
-let totalWorkingDays = 0;
-
-while (totalWorkingDays < NO_OF_WORKING_DAYS && TotalEmpHrs <= MAX_WORKING_HRS){
-    let empCheck = Math.round(Math.random() * 10) % 3;
-    TotalEmpHrs += getEmpHrs(empCheck);
-    totalWorkingDays++;
+function calculateDailyWage(empHrs){
+    return empHrs * WAGE_PER_HR;
 }
 
-empWage = TotalEmpHrs * WAGE_PER_HR;
+while (totalWorkingDays < NO_OF_WORKING_DAYS && TotalEmpHrs <= MAX_WORKING_HRS){
+    totalWorkingDays++;
+    let empCheck = Math.round(Math.random() * 10) % 3;
+    empHrs = getEmpHrs(empCheck);
+    TotalEmpHrs += empHrs;
+    empDailyWageArr.push(calculateDailyWage(empHrs));
+ 
+}
+
+empWage = calculateDailyWage(TotalEmpHrs);
 
 console.log(`total woorking days ${totalWorkingDays} total hours of working ${TotalEmpHrs} Employee wage is ${empWage}`);
+console.log(empDailyWageArr);
